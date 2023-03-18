b2efff97eebff680738738b16c94d33fc28c086d
Kieran Collins <kiecol@protonmail.com>
2023-03-18 16:16:49 +0000

    Set up template inhertiance

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialization
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
+  
